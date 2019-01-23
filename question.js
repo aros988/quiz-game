00023fdc4d16b5652f011fb6b90dec8cacdfddbe
Arkadiusz Krezel <arkadiuszkrezel88@gmail.com>
2019-01-23 19:24:27 +0100

    first commit

---
+++ question.js
@@ -0,0 +1,58 @@
+var questions = [{
+    "question": "What kind of Java Script using React.js?",
+    "option1": "JS",
+    "option2": "JSL",
+    "option3": "JSJ",
+    "option4": "JSX",
+    "answer": "4"
+},{
+    "question": "Inside which HTML element do we put the JavaScript?",
+    "option1": "<scripting>",
+    "option2": "<script>",
+    "option3": "<javascript>",
+    "option4": "<js>",
+    "answer": "2"
+},{
+    "question": "Where is the correct place to insert a JavaScript?",
+    "option1": "The <head> section",
+    "option2": "Both the <head> section and the <body> section are correct",
+    "option3": "The <title> section",
+    "option4": "The <body> section",
+    "answer": "2"
+},{
+    "question": "Which sign does jQuery use as a shortcut for jQuery?",
+    "option1": "the $ Sign",
+    "option2": "the % Sign",
+    "option3": "the ? Sign",
+    "option4": "the # Sign",
+    "answer": "1"
+},{
+    "question": "Choose the correct HTML element for the largest heading?",
+    "option1": "<h6>",
+    "option2": "<h1>",
+    "option3": "<heading>",
+    "option4": "<head>",
+    "answer": "2"
+},{
+    "question": " Choose the correct HTML element to define important text?",
+    "option1": "<strong>",
+    "option2": "<i>",
+    "option3": "<b>",
+    "option4": "<important>",
+    "answer": "1"
+},{
+    "question": "What does CSS stand for?",
+    "option1": "Colorful Style Sheets",
+    "option2": "Compile Style Sheets",
+    "option3": "Cascading Style Sheets",
+    "option4": "Computer Style Sheets",
+    "answer": "3"
+},{
+    "question": " Which of these elements are all <table> elements?",
+    "option1": "<table><tr><tt>",
+    "option2": "<thead><body><tr>",
+    "option3": "<table><tr><td>",
+    "option4": "<table><head><tfoot>",
+    "answer": "3"
+}
+];
